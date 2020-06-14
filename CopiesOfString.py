94abba9ced2bcd0ab1286f8b408f95e4d8b1dc3f
Dipanjan Saha <sahadipanjan6@gmail.com>
2020-06-14 14:45:15 +0530

    Create CopiesOfString.py

---
+++ CopiesOfString.py
@@ -0,0 +1,8 @@
+''' Question: Write a Python program to get a string which is n (non-negative integer) copies of a given string.
+
+Solution: '''
+
+get_string = input('Enter any string: ')
+value_of_n = int(input('Enter the value of n: '))
+output_string = get_string * value_of_n
+print("Output String: ", output_string)
