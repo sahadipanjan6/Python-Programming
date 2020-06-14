10fe9688d9e0c10536cfe8bb32c6ce2c5eca9047
Dipanjan Saha <sahadipanjan6@gmail.com>
2020-06-14 16:06:23 +0530

    Create GetASCII.py

---
+++ GetASCII.py
@@ -0,0 +1,6 @@
+''' Question: Write a Python program to get the ASCII value of a character.
+
+Solution: '''
+
+character = input('Enter the character: ')
+print("ASCII value of {} is {}".format(character, ord(character)))
