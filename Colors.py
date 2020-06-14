c663dc2184287f8ab9845066d3c95cb889a649cf
Dipanjan Saha <sahadipanjan6@gmail.com>
2020-06-14 14:35:41 +0530

    Create Colors.py

---
+++ Colors.py
@@ -0,0 +1,8 @@
+''' Question: Write a Python program to display the first and last colors from the following list: 
+[“Red”, “Green”, “White”, “Black”]
+
+Solution: '''
+
+given_list = ["Red", "Green", "White", "Black"]
+print("First Color: ", given_list[0])
+print("Last Color: ", given_list[len(given_list)-1])
