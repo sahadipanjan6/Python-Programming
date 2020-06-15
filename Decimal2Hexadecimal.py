dab109b42122547812acf8e946e8172df43b151a
Dipanjan Saha <sahadipanjan6@gmail.com>
2020-06-15 08:41:32 +0530

    Create Decimal2Hexadecimal.py

---
+++ Decimal2Hexadecimal.py
@@ -0,0 +1,6 @@
+''' Question: Write a python program to convert decimal to hexadecimal.
+
+Solution: '''
+
+decimal_number = int(input('Enter a decimal number: '))
+print("{} is the hexadecimal equivalent of {}.".format(hex(decimal_number), decimal_number))
