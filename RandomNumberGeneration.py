c5d4e68dea86a5832097bddec398b1df263618b3
Dipanjan Saha <sahadipanjan6@gmail.com>
2020-06-15 11:06:47 +0530

    Create RandomNumberGeneration.py

---
+++ RandomNumberGeneration.py
@@ -0,0 +1,12 @@
+''' Question: Write a Python program to generate a Random Number.
+
+Solution: '''
+
+from random import random
+
+def print_random():
+    print(random())
+    
+#entry point of the program
+if __name__ == '__main__':
+    print_random()
