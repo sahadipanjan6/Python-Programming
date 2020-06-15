282496c1edf14e231a0e3023eb66f4c763911a65
Dipanjan Saha <sahadipanjan6@gmail.com>
2020-06-15 08:42:17 +0530

    Create CheckSetOrTuple.py

---
+++ CheckSetOrTuple.py
@@ -0,0 +1,11 @@
+''' Question: Write a Python program to test if a variable is a list or tuple or a set.
+
+Solution: '''
+
+variable1 = [1, 2, 3]
+variable2 = (2, 5)
+variable3 = set([1, 2, 2, 3])
+
+print("Type of variable1: ", type(variable1))
+print("Type of variable2: ", type(variable2))
+print("Type of variable3: ", type(variable3))
